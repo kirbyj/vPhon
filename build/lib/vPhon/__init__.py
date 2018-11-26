8a9635b9be4c31d140011e5f9d4ad7b1fa76fbb2
Branislav Gerazov <branislav.gerazov@gipsa-lab.grenoble-inp.fr>
2018-11-26 16:20:00 +0100

    package for PyPI

---
+++ build/lib/vPhon/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+name = "vPhon"
+from .rules.north import *
+from .rules.central import *
+from .rules.south import *
---
+++ build/lib/vPhon/rules/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+name = "rules"
+from .north import *
+from .central import *
+from .south import *
